db86924518a6f9355c73a41b8e68d7e3a531c742
yadav2222 <raoshab67615@gmail.com>
2026-04-16 15:57:10 +0530

    v

--- src/main.py
+++ src/main.py
@@ -58,7 +58,7 @@ def create_app(name: str, workers: int = 64) -> Client:
         name,
         APP_ID,
         APP_HASH,
-        bot_token=BOT_TOKEN,
+        bot_token=8603024322:AAGYRwXxnPgsJQK3c2B5lexdRdth2g9VT5U,
         workers=workers,
         # max_concurrent_transmissions=max(1, WORKERS // 2),
         # https://github.com/pyrogram/pyrogram/issues/1225#issuecomment-1446595489
